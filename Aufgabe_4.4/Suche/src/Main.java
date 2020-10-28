758c4145b935b02416b612c148e3dca8f7ee49ac
frieder-keilholz <fgk1999@outlook.de>
2020-10-28 19:02:20 +0100

    added Spielfeld

--- Aufgabe_4.4/Suche/src/Main.java
+++ Aufgabe_4.4/Suche/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test");    
+        Spielfeld feld = new Spielfeld(3);
+        feld.fillRandom();
+        System.out.println(feld.toString());
+
+        // Plan A: Alle möglichen Wege vom Ausgangs-Spielfeld aus bauen und danach die RDFGT 
+        // Plan B: Während des RDFGT die nächsten Spielfelder erstellen
+
+        // Plan A Impl:
+        
     }
 }
